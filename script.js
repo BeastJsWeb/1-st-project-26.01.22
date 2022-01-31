83f2a5697367cd0d3567df31dcbc5f5e99b79759
Vitaliy <vitalijivanov1832@gmail.com>
2022-01-31 18:35:46 +0700

    -

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const nav = document.getElementById('js-nav');
 const navOff = document.getElementById('js-nav--off');
 const main = document.getElementById('js-main');
 
-navOff.onclick = () => nav.classList.toggle('js-nav--on');
+navOff.addEventListener('click', () => nav.classList.toggle('js-nav--on'));
 
-main.onclick = () => nav.classList.remove('js-nav--on');
+main.addEventListener('click', () => nav.classList.remove('js-nav--on'));
 
 // MAIN MIDDLE
 
@@ -19,81 +19,83 @@ const statusBtn2 = document.getElementById('js-button-2');
 const statusBtn3 = document.getElementById('js-button-3');
 const statusBtn4 = document.getElementById('js-button-4');
 
-statusBtn1.onclick = () => {
+statusBtn1.addEventListener('click', () => {
   statusBtn1.classList.add('js-button--active');
   statusBtn2.classList.remove('js-button--active');
   statusBtn3.classList.remove('js-button--active');
   statusBtn4.classList.remove('js-button--active');
-};
-statusBtn2.onclick = () => {
+});
+statusBtn2.addEventListener('click', () => {
   statusBtn2.classList.add('js-button--active');
   statusBtn1.classList.remove('js-button--active');
   statusBtn3.classList.remove('js-button--active');
   statusBtn4.classList.remove('js-button--active');
-};
-statusBtn3.onclick = () => {
+});
+statusBtn3.addEventListener('click', () => {
   statusBtn3.classList.add('js-button--active');
   statusBtn1.classList.remove('js-button--active');
   statusBtn2.classList.remove('js-button--active');
   statusBtn4.classList.remove('js-button--active');
-};
-statusBtn4.onclick = () => {
+});
+statusBtn4.addEventListener('click', () => {
   statusBtn4.classList.add('js-button--active');
   statusBtn1.classList.remove('js-button--active');
   statusBtn2.classList.remove('js-button--active');
   statusBtn3.classList.remove('js-button--active');
-};
+});
 
 // 2-2-4 MIDDLE GRADE
 
-const grade1 = document.querySelector('.js-grade-1'),
-  grade2 = document.querySelector('.js-grade-2'),
-  grade3 = document.querySelector('.js-grade-3'),
-  grade4 = document.querySelector('.js-grade-4'),
-  grade5 = document.querySelector('.js-grade-5');
+const grade = document.querySelector('.middle__grade'),
+  grade1 = grade.querySelector('.js-grade-1'),
+  grade2 = grade.querySelector('.js-grade-2'),
+  grade3 = grade.querySelector('.js-grade-3'),
+  grade4 = grade.querySelector('.js-grade-4'),
+  grade5 = grade.querySelector('.js-grade-5');
 
-grade1.onclick = () => {
+grade1.addEventListener('click', () => {
   grade1.classList.add('js-grade--on');
   grade2.classList.remove('js-grade--on');
   grade3.classList.remove('js-grade--on');
   grade4.classList.remove('js-grade--on');
   grade5.classList.remove('js-grade--on');
-};
-grade2.onclick = () => {
+});
+grade2.addEventListener('click', () => {
   grade2.classList.add('js-grade--on');
   grade1.classList.remove('js-grade--on');
   grade3.classList.remove('js-grade--on');
   grade4.classList.remove('js-grade--on');
   grade5.classList.remove('js-grade--on');
-};
-grade3.onclick = () => {
+});
+grade3.addEventListener('click', () => {
   grade3.classList.add('js-grade--on');
   grade1.classList.remove('js-grade--on');
   grade2.classList.remove('js-grade--on');
   grade4.classList.remove('js-grade--on');
   grade5.classList.remove('js-grade--on');
-};
-grade4.onclick = () => {
+});
+grade4.addEventListener('click', () => {
   grade4.classList.add('js-grade--on');
   grade1.classList.remove('js-grade--on');
   grade2.classList.remove('js-grade--on');
   grade3.classList.remove('js-grade--on');
   grade5.classList.remove('js-grade--on');
-};
-grade5.onclick = () => {
+});
+grade5.addEventListener('click', () => {
   grade5.classList.add('js-grade--on');
   grade1.classList.remove('js-grade--on');
   grade2.classList.remove('js-grade--on');
   grade3.classList.remove('js-grade--on');
   grade4.classList.remove('js-grade--on');
-};
+});
+
 // 2-2-6 MIDDLE NOTE
 
 const textarea = document.getElementById('js-note__textarea');
 const note = document.getElementById('js-note');
 const areaBtn = document.querySelectorAll('.js-textarea__button');
 
-note.onclick = () => textarea.classList.add('js-note__textarea--on');
+note.addEventListener('click', () => textarea.classList.add('js-note__textarea--on'));
 
 areaBtn.forEach((areaBtn) => areaBtn.onclick = () => {
   textarea.classList.remove('js-note__textarea--on');
@@ -106,14 +108,14 @@ const actorsList = document.getElementById('js-actors__list');
 const descrBtn = document.getElementById('js-description__button');
 const actorsBtn = document.getElementById('js-actors__button');
 
-descrBtn.onclick = () => {
+descrBtn.addEventListener('click', () => {
   descrList.classList.add('js-description__list--on');
   actorsList.classList.remove('js-actors__list--on');
-};  
-actorsBtn.onclick = () => {
+});  
+actorsBtn.addEventListener('click', () => {
   actorsList.classList.add('js-actors__list--on');
   descrList.classList.remove('js-description__list--on');
-};
+});
 
 //--4-0 (2-1-1) THEME BLACK--------------------
 
